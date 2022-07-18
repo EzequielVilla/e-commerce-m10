d5eb80eb26d0c881265a2375f88cd74de45032bd
Eze <ezequiel.n.villa@gmail.com>
2022-07-18 11:39:43 -0300

    change global.css place

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import "lib/base.css";
+import "styles/base.css";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
